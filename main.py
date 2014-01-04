50a1e5e5b11aedefaf723fc7a572596257421f08
ingelres <github@fingelrest.net>
2014-01-04 08:12:41 +0100

    Removed the useless nbPlayer command-line argument

--- main.py
+++ main.py
@@ -13,8 +13,7 @@ from sixtakes import SixTakes
 availablePlayers = 'Available players:\n\n' + '\n'.join(['    %-15s %s' % (name, inst.desc()) for (name, inst) in playerstype.getAvailablePlayers()])
 mParser          = ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, epilog=availablePlayers)
 
-mParser.add_argument('nbPlayers', metavar='NB_PLAYERS', type=int, help='Must be between 2 and 10')
-mParser.add_argument('types', metavar='PLAYER', type=str, nargs='+', help='A player must be specified for each player in the game')
+mParser.add_argument('players', metavar='PLAYER', type=str, nargs='+', help='A player must be specified for each player in the game (at least 2)')
 mParser.add_argument('-d', '--display', dest='display', action='store_true', help='Display the game proceedings (defaults to true when at least one player is human)')
 mParser.add_argument('-i', '--iterations', dest='nbIters', type=int, default=1, help='Number of iterations (defaults to 1)')
 
@@ -24,14 +23,11 @@ mArgs   = mParser.parse_args()
 players = []
 
 # Check the number of players
-if mArgs.nbPlayers < 2 or mArgs.nbPlayers > 10:
-    mParser.error('The number of players is invalid')
+if len(mArgs.players) < 2 or len(mArgs.players) > 10:
+    mParser.error('The number of players must be between 2 and 10')
 
-# Check that a valid player type has been chosen for each player
-if len(mArgs.types) != mArgs.nbPlayers:
-    mParser.error('A valid PLAYER must be chosen for each player')
-
-for type in mArgs.types:
+# Check that all players are valid
+for type in mArgs.players:
     try:
         players.append(playerstype.getAvailablePlayers()[[player[0] for player in playerstype.getAvailablePlayers()].index(type)])
     except:
@@ -42,8 +38,8 @@ if mArgs.display: display = ui.cursesui.CursesUI()
 else:             display = ui.DummyUI()
 
 game       = SixTakes(players, display)
-victories  = [0 for i in xrange(mArgs.nbPlayers)]
-blockheads = [0 for i in xrange(mArgs.nbPlayers)]
+victories  = [0 for i in xrange(len(mArgs.players))]
+blockheads = [0 for i in xrange(len(mArgs.players))]
 
 if not mArgs.display:
     print 'Simulations in progress...'
