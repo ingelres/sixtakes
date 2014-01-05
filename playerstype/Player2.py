c62851709847719f98517788c5c42b38717c6e1b
ingelres <github@fingelrest.net>
2014-01-05 07:34:46 +0100

    Added two new players

---
+++ playerstype/Player2.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+#
+# Author: François Ingelrest
+
+import playerstype, random
+
+from playerstype import Player
+
+
+class Player2(playerstype.Player):
+
+
+    def __init__(self):
+        """ Constructor """
+        pass
+
+
+    def desc(self):
+        return 'Play the shortest row'
+
+
+    def play(self, table, hand):
+        """
+            Play the card that should go in the row with the smallest number of cards.
+        """
+        best = (9999, -1)    # (score, index)
+
+        for hidx, hcard in enumerate(hand):
+            for tidx, trow in enumerate(table):
+
+                diff = hcard - trow[-1]
+
+                if diff > 0: score = len(trow) * 1000 + diff
+                else:        score = 6000 + hcard
+
+                if score < best[0]:
+                    best = (score, hidx)
+
+        return best[1]
+
+
+
+    def take(self, table, playerstype):
+        """
+            Choose the row with the smallest number of blockheads
+        """
+        return Player.getSmallestRow(self, table)
+
